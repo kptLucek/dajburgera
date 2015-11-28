bd062284215034c6f3912654221126b47244c3ad
Marcin Bonk <marcin.bonk@docplanner.com>
2015-11-28 12:34:29 +0100

    updated orm

---
+++ api/src/AppBundle/Entity/Restaurant.php
@@ -0,0 +1,287 @@
+<?php
+
+namespace AppBundle\Entity;
+
+/**
+ * Restaurant
+ */
+class Restaurant
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var float
+     */
+    private $lon;
+
+    /**
+     * @var float
+     */
+    private $lat;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string
+     */
+    private $city;
+
+    /**
+     * @var string
+     */
+    private $street;
+
+    /**
+     * @var string
+     */
+    private $postalCode;
+
+    /**
+     * @var string
+     */
+    private $buildingNumber;
+
+    /**
+     * @var string
+     */
+    private $apartmentNumber;
+
+    /**
+     * @var string
+     */
+    private $country;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set lon
+     *
+     * @param string $lon
+     *
+     * @return Restaurant
+     */
+    public function setLon($lon)
+    {
+        $this->lon = $lon;
+
+        return $this;
+    }
+
+    /**
+     * Get lon
+     *
+     * @return string
+     */
+    public function getLon()
+    {
+        return $this->lon;
+    }
+
+    /**
+     * Set lat
+     *
+     * @param string $lat
+     *
+     * @return Restaurant
+     */
+    public function setLat($lat)
+    {
+        $this->lat = $lat;
+
+        return $this;
+    }
+
+    /**
+     * Get lat
+     *
+     * @return string
+     */
+    public function getLat()
+    {
+        return $this->lat;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Restaurant
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set city
+     *
+     * @param string $city
+     *
+     * @return Restaurant
+     */
+    public function setCity($city)
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    /**
+     * Get city
+     *
+     * @return string
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    /**
+     * Set street
+     *
+     * @param string $street
+     *
+     * @return Restaurant
+     */
+    public function setStreet($street)
+    {
+        $this->street = $street;
+
+        return $this;
+    }
+
+    /**
+     * Get street
+     *
+     * @return string
+     */
+    public function getStreet()
+    {
+        return $this->street;
+    }
+
+    /**
+     * Set postalCode
+     *
+     * @param string $postalCode
+     *
+     * @return Restaurant
+     */
+    public function setPostalCode($postalCode)
+    {
+        $this->postalCode = $postalCode;
+
+        return $this;
+    }
+
+    /**
+     * Get postalCode
+     *
+     * @return string
+     */
+    public function getPostalCode()
+    {
+        return $this->postalCode;
+    }
+
+    /**
+     * Set buildingNumber
+     *
+     * @param string $buildingNumber
+     *
+     * @return Restaurant
+     */
+    public function setBuildingNumber($buildingNumber)
+    {
+        $this->buildingNumber = $buildingNumber;
+
+        return $this;
+    }
+
+    /**
+     * Get buildingNumber
+     *
+     * @return string
+     */
+    public function getBuildingNumber()
+    {
+        return $this->buildingNumber;
+    }
+
+    /**
+     * Set apartmentNumber
+     *
+     * @param string $apartmentNumber
+     *
+     * @return Restaurant
+     */
+    public function setApartmentNumber($apartmentNumber)
+    {
+        $this->apartmentNumber = $apartmentNumber;
+
+        return $this;
+    }
+
+    /**
+     * Get apartmentNumber
+     *
+     * @return string
+     */
+    public function getApartmentNumber()
+    {
+        return $this->apartmentNumber;
+    }
+
+    /**
+     * Set country
+     *
+     * @param string $country
+     *
+     * @return Restaurant
+     */
+    public function setCountry($country)
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    /**
+     * Get country
+     *
+     * @return string
+     */
+    public function getCountry()
+    {
+        return $this->country;
+    }
+}
+

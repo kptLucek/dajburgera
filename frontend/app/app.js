d9561cf5c7cbeedc8659df1e6fc26b39c7725246
Bartosz Bujakowski <bartosz.bujakowski@decerto.pl>
2015-11-28 16:45:09 +0100

    Burger merge

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -3,7 +3,15 @@
 
 	angular.module('myapp').controller('mainCtrl', mainCtrl);
 
-	function mainCtrl() {
+	function mainCtrl($scope, $timeout) {
+
+		$scope.showBurger = false;
+
+		$scope.giveMeBurger = function() {
+			$timeout(function () {
+				$scope.showBurger = true;
+			}, 3000);
+		};
 
 	}
 

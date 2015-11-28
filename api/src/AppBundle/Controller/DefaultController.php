8f009139dd271bca55ffcb3ba6b2ad17526b8355
Marcin Bonk <marcin.bonk@docplanner.com>
2015-11-28 14:00:37 +0100

    Bit of logic

--- api/src/AppBundle/Controller/DefaultController.php
+++ api/src/AppBundle/Controller/DefaultController.php
@@ -2,9 +2,10 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Restaurant;
+use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Controller\FOSRestController;
 use Symfony\Component\HttpFoundation\Request;
-use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends FOSRestController
@@ -20,7 +21,8 @@ class DefaultController extends FOSRestController
     {
         $view = $this->view([
             'status' => 'ok',
-            'code' => 200
+            'code' => 200,
+//            'data' => $this->get('fos_elastica.finder.burger.restaurant')->find();
         ]);
         return $this->handleView($view);
     }
@@ -33,6 +35,27 @@ class DefaultController extends FOSRestController
      */
     public function putBurgerAction(Request $request)
     {
-        return $this->view([]);
+        $postParams = $request->request->all();
+        $validator = $this->get('validator');
+        $propertyAccessor = $this->get('property_accessor');
+        $entry = new Restaurant();
+        foreach ($postParams as $key => $value) {
+            if (false === $propertyAccessor->isWritable($entry, $key)) {
+                return $this->handleView($this->view([
+                    'status' => 'error',
+                    'code' => Response::HTTP_METHOD_NOT_ALLOWED,
+                ]));
+            }
+            $propertyAccessor->setValue($entry, $key, $value);
+            $propertyValid = $validator->validateProperty($entry, $key);
+            if (0 < $propertyValid->count()) {
+                return $this->handleView($this->view($propertyValid));
+            }
+        }
+
+        return $this->view([
+            'status' => 'ok',
+            'code' => Response::HTTP_OK
+        ]);
     }
 }
